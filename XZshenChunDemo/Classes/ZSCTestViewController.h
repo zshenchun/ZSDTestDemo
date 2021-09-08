f6a68f833dc09f3cb35b159dd3910d09b456e299
apple <apple@appledeMac-mini.local>
2021-09-08 11:52:54 +0800

    fist commit

---
+++ XZshenChunDemo/Classes/ZSCTestViewController.h
@@ -0,0 +1,16 @@
+//
+//  ZSCTestViewController.h
+//  XZshenChunDemo
+//
+//  Created by apple on 2021/9/8.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ZSCTestViewController : UIViewController
+- (void)testPrint;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XZshenChunDemo/Classes/ZSCTestViewController.m
@@ -0,0 +1,33 @@
+//
+//  ZSCTestViewController.m
+//  XZshenChunDemo
+//
+//  Created by apple on 2021/9/8.
+//
+
+#import "ZSCTestViewController.h"
+
+@interface ZSCTestViewController ()
+
+@end
+
+@implementation ZSCTestViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (void)testPrint{
+    NSLog(@"我的Pod第一次打印");
+}
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
